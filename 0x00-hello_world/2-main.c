4e0ca5949b283b9d58950c46f0260236629820a8
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-11 01:56:13 -0800

    new message

--- 0x00-hello_world/2-main.c
+++ 0x00-hello_world/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        printf("Holberton School");
-	return 0;
+	printf("Holberton School");
+	return (0);
 }
-
