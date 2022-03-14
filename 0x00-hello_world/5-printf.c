a61283d05279edc5f60a593470d3619f214001c0
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-14 13:59:33 -0700

    mmessage

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(void)
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
