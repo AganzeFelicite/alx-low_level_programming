cc075a640d63092bf2672625e557192ffb843c1a
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-14 15:08:25 -0700

    hot fixes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 printf("Size of a char: 1 byte(s)\n");
 printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 4 byte(s)\n");
-printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a long int: %d byte(s)\n" ,sizeof(long));
+printf("Size of a long long int: %d byte(s)\n",sizeof(long long));
 printf("Size of a float: 4 byte(s)\n");
 return (0);
 }
