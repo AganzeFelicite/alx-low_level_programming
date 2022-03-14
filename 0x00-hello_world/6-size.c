e4c95f5d67d355cc6bf1973107319cfe5a575431
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-14 14:27:50 -0700

    first attempt

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: 1 byte(s)");
+printf("Size of an int: 4 byte(s)");
+printf("Size of a long int: 4 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+prinft("Size of a float: 4 byte(s)");
+return (0);
+}
