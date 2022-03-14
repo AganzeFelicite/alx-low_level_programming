6b4ab2d86cd8b38cfb41cd8b999203fe13486e47
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-14 14:30:01 -0700

    second attempt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ printf("Size of a char: 1 byte(s)");
 printf("Size of an int: 4 byte(s)");
 printf("Size of a long int: 4 byte(s)");
 printf("Size of a long long int: 8 byte(s)");
-prinft("Size of a float: 4 byte(s)");
+printf("Size of a float: 4 byte(s)");
 return (0);
 }
