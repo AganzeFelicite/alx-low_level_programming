4effaf77b6214afe81c5fd0e6c4727bc0560cb7c
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-21 08:16:03 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * this function Write a function that takes a pointer to an int as parameter and u *pdates the value it points to to 98.
+ *
+ */
+
+ void reset_to_98(int *n)
+{
+    *n = 98;
+
+}
