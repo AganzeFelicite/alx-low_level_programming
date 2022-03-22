0caf8b3fe47bd390915671b01e234c2a1488fdf1
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-22 03:37:21 -0700

    new

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -find the length of a string
+ * @s:pointer to the string
+ * Return :int
+*/
+
+int _strlen(char *s)
+{
+ int i;
+ i = strlen(s);
+ return (i);
+}
