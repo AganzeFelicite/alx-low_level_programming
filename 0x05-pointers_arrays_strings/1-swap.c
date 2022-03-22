9110834cfe5d006a5f6c0a88bf33250ed3638ba4
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-22 03:04:56 -0700

    123456789

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int -swap variables values
+ * @a:pointer one
+ * @b:pointer two
+ * Return void
+*/
+void swap_int (int *a,int *b)
+{
+int temp = *a;
+*a =*b;
+*b =temp;
+}
