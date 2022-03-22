b387ba7682a6b04616d06ea76a97f060f48a7ee8
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-22 02:36:47 -0700

    new commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * this function Write a function that takes
- * reset_to_98 -function
- * @n:int to be checked
- */
- void reset_to_98(int *n)
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
+*/
+void reset_to_98(int *n)
 {
 *n = 98;
 }
