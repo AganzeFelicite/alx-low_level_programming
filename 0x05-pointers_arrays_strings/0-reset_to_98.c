4515eaf30d4b52130f8cdb7633a7d74a019a6ff5
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-22 02:28:53 -0700

    new

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,10 @@
 
 /**
  * this function Write a function that takes
- * reset_to_98 function
- * function void reset_to_98(int *n)
+ * reset_to_98 -function
+ * @n:int to be checked
  */
-
  void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
