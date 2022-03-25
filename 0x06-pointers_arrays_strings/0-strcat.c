2b7117fb0d238982b10a81deb23858e0417eaad4
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-25 01:23:22 -0700

    string concatination number 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - string concatination function
+ * @dest: the poointer to the first string
+ * @src: pointer to the second string
+ * Return: void
+**/
+char *_strcat(char *dest,char *src)
+{
+int i =0;
+int len1 = 0;
+int  len2 = 0;
+while(dest[len1])
+{
+	len1++;
+}
+while(src[len2])
+{
+	len2++;
+}
+for(i=0;i<=len2;i++)
+{
+dest[len1+i] = src[i];
+}
+return(dest);
+}
