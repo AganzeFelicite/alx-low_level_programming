6d6d04bc67d6576e2d5fe5077964d0fa168d1e02
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-29 07:33:44 -0700

    hot fixes in betty for my file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include "main.h"
+
 /**
  * _memeset - function to fill the memory allocationts withconstants byte
  * @s:pointer to memory area
@@ -8,6 +8,7 @@
  * @n: unsigned integer
  * Return: s
 **/
+
 
 char *_memset(char *s, char b, unsigned int n)
 {
