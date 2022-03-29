1ff10aafcbf1d1f83a1718bf7e26ebcff3d6ae23
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-29 08:23:05 -0700

    Error free code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memeset - function to fill the memory allocationts withconstants byte
+ * _memset - function to fill the memory allocationts withconstants byte
  * @s: pointer to memory area
  * @b: a char variable
  * @n: unsigned integer
@@ -12,9 +12,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
-for (i = 0;i < n; i++)
+for (i = 0; i < n; i++)
 {
-s[i] = b; 
+s[i] = b;
+
 }
 return (s);
 }
