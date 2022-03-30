684db4619eac704d3db2aab28288153716a0a334
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-30 21:10:13 +0200

    hot fixe

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _sqrt_recursion - find natural square root
--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * last_index - returns the last index of a string (counts the null char)
