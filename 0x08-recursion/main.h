e42ddcda2d82e910aab6b6204170dc6817e053ef
AganzeFelicite <aganzefelicite@gmail.com>
2022-03-30 21:06:44 +0200

    new commit all in one

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,2 +1,13 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int square(int n, int val);
+int _sqrt_recursion(int n);
+int check_prime(int n, int resp);
+int is_prime_number(int n);
+int last_index(char *s);
+int check(char *s, int start, int end, int pair);
+int is_palindrome(char *s);
