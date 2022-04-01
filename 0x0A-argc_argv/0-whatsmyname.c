8715a568b11a8467b58d94602e458fb1efa4ca37
AganzeFelicite <aganzefelicite@gmail.com>
2022-04-01 05:00:17 -0700

    first commmit on this

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - the main function
+ * @argv: an array on element entered
+ * @argc: number of elemnet entered
+ * Return: 0
+**/
+int main(int argc, char *argv[])
+{
+(void) argc;
+
+printf("%s\n", argv[0]);
+
+return (0);
+
+}
