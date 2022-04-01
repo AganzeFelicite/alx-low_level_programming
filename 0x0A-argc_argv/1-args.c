ec1e365eaad74f7c155bb573856adf593b4bc9ae
AganzeFelicite <aganzefelicite@gmail.com>
2022-04-01 05:35:12 -0700

    second commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - its the main function of my codoe
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: int
+**/
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+
+printf("%d\n", argc);
+return (0);
+}
