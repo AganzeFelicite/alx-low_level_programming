32b2768961c76fdff8f45bb8e0648773dd8c922f
AganzeFelicite <aganzefelicite@gmail.com>
2022-04-01 06:31:51 -0700

    second question

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -function
+ * @argc: number ofinputs
+ * @argv: array of inputs
+ * Return: int
+**/
+
+
+int main(int argc, char *argv[])
+{
+
+int i;
+
+for (i = 1; i < argc; i++)
+
+{ 
+
+printf("%s\n", argv[i]);
+
+}
+
+return (0);
+
+}
+
+
