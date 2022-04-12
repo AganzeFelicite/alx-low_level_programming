fca9a0b35842107df556c1dd44235c0defa4b2e0
AganzeFelicite <aganzefelicite@gmail.com>
2022-04-12 04:17:35 -0700

    multidimensional array erro free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * alloc_grid - function to return a two dimenssional aray
+ * @width: width size
+ * @height: size
+ * Return: a multidimensional array
+ */
+
+
+int **alloc_grid(int width, int height)
+{	int i, j;
+	int **p;
+
+	while (width <= 0 || height <= 0)
+		return (NULL);
+	p = (int **)malloc(sizeof(int) * height);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		p[i] = (int *)malloc(sizeof(int) * width);
+		if (p[i] == NULL)
+		{	free(p);
+			for (j = 0; j < height; j++)
+			{
+				free(p[j]);
+			}
+			return (NULL);
+		}
+	}
+	for (i = 0 ; i < height; i++)
+		for (j = 0 ; j < width; j++)
+			p[i][j] = 0;
+	return (p);
+
+}
