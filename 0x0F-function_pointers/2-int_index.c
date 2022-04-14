98e11e7f2bc1e0415447a0eac3d562399524cc65
AganzeFelicite <aganzefelicite@gmail.com>
2022-04-13 17:11:01 -0700

    hot fixees on this file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,8 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i;
 if (array && cmp)
 	for (i = 0; i < size; i++)
-		if (cmp(array[i] != 0)
-				return (i);
-		return (-1);
+	if (cmp(array[i]) != 0)
+		return (i);
+return (-1);
 
 }
