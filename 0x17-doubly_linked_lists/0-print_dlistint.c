97b818b74a31035de4eae758859631b0f09a0ea7
AGANZE FELICITE <99313069+AganzeFelicite@users.noreply.github.com>
2022-06-10 13:56:58 +0200

    new betty error fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
-/*
- * print_dlistint - a function to print element of the linked list
- * @h: this is head pointer
- * Return: unsigned int value
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count;
-	count = 0;
-	if (!h)
-		return (0);
+	size_t nodes = 0;
+
 	while (h)
 	{
-		count++;
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-return (count);
 }
