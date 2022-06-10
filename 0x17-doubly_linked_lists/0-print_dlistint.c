ededa65a8fc18cf33a8f19bd87bf81b03b29213f
AganzeFelicite <aganzefelicite@gmail.com>
2022-06-10 04:28:00 -0700

    addition of the folder

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/*
+ * print_dlistint - a function to print element of the linked list
+ * @h: this is head pointer
+ * Return: unsigned int value
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+	count = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count += 1;
+	}
+return(count);
+}
