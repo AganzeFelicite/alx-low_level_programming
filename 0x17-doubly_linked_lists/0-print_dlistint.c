ebc7033eb84b9b794c5f2386003aa0df2ac1c9e5
AGANZE FELICITE <99313069+AganzeFelicite@users.noreply.github.com>
2022-06-10 13:51:49 +0200

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,12 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count;
 	count = 0;
 	if (!h)
-	{
 		return (0);
-	}
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count += 1;
+		count++;
 	}
 return (count);
 }
