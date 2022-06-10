a86cc497e37072b6dcaca55b2efc28e73a89d890
AganzeFelicite <aganzefelicite@gmail.com>
2022-06-10 17:09:05 +0200

    quick ccommit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,30 +1,27 @@
 #include "lists.h"
-/*
- * add_dnodeint  -  function to add anode at the beginning
- * @head: pointer tp the first node
- * @n: the value to go in the node
- * Return: a node
+
+/**
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
 
-	if (!*head)	/* check if head is NULL */
-		*head = new_node;
-	else	/* head has nodes */
-	{
-		new_node->next = *head;
-		(*head)->prev = new_node;	/* update prev pointer */
-		*head = new_node;
-	}
-	return (new_node);
-
+	return (new);
 }
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -1,2 +1,5 @@
-THIS ABOUT DOUBLY LINKED LISTS IN C PROGRAMMMING 
- 
+# ALX SE Program - Cohort 5
+# Doubly linked Lists
+
+This project was done in the sprint 2 of [Python] 
+But it is included in the alx-low_level_programming C sprint 1.
