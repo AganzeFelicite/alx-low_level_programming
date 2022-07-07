8afd46a553f9c186f856de12a416419f9fd2d6c0
AganzeFelicite <aganzefelicite@gmail.com>
2022-07-07 16:31:57 +0200

    a nother bug fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	new_node->value = strdup(value);
+	new_node->value = new_value;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
