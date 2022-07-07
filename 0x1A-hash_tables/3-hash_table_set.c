70315f579c8ccc56040f6e4c0acb5a9c2eb79d00
AganzeFelicite <aganzefelicite@gmail.com>
2022-07-07 16:22:02 +0200

    a hot fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,12 +34,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_value);
 		return (0);
 	}
+	 new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
